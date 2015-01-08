a12ee79559a446faa655c8a25a6b61a0de893cfe
Maggie-M <maggie.mari@continuum.io>
2015-01-08 12:39:01 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='HelloBinstar',
+      version='0.1',
+      description='A sample package',
+      url='http://github.com/me/HelloBinstar',
+      author='Maggie',
+      author_email='me@example.com',
+      license='BSD',
+      packages=['HelloBinstar'],
+      zip_safe=False)
